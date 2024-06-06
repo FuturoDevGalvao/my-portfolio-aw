280894ece707e07d0a899f18127b3a1eacb4d2f1
FuturoDevGalvao <inosuke567765@gmail.com>
2024-06-06 14:46:03 -0300

    feat: layout retrabahado e modularização da lógica do carrosel efetuadas

---
+++ dist/carrosel.js
@@ -0,0 +1,19 @@
+var copy = document.querySelector(".lenguages-logo-slide").cloneNode(true);
+document.querySelector(".lenguages-logo").appendChild(copy);
+document.querySelector(".lenguages-logo").appendChild(copy);
+document.querySelector(".lenguages-logo").appendChild(copy);
+document.querySelector(".lenguages-logo").appendChild(copy);
+document.querySelector(".lenguages-logo").appendChild(copy);
+document.querySelector(".lenguages-logo").appendChild(copy);
+
+copy = document.querySelector(".frameworks-logo-slide").cloneNode(true);
+document.querySelector(".frameworks-logo").appendChild(copy);
+document.querySelector(".frameworks-logo").appendChild(copy);
+document.querySelector(".frameworks-logo").appendChild(copy);
+document.querySelector(".frameworks-logo").appendChild(copy);
+
+copy = document.querySelector(".thecnology-logo-slide").cloneNode(true);
+document.querySelector(".thecnology-logo").appendChild(copy);
+document.querySelector(".thecnology-logo").appendChild(copy);
+document.querySelector(".thecnology-logo").appendChild(copy);
+document.querySelector(".thecnology-logo").appendChild(copy);
